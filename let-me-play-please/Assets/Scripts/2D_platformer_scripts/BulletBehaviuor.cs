23f2f5b1b338656be0eb908085109c68faf48c6a
fhps <higino.felipe@hotmail.com>
2020-07-11 12:54:15 -0300

    Bullet auto destruction
    Bullet destroyed after 3 seconds.

--- let-me-play-please/Assets/Scripts/2D_platformer_scripts/BulletBehaviuor.cs
+++ let-me-play-please/Assets/Scripts/2D_platformer_scripts/BulletBehaviuor.cs
@@ -6,12 +6,17 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class BulletBehaviuor : MonoBehaviour
 {
+    //references
     [SerializeField] private float bullet_speed = 20f;
     private Rigidbody2D bulletrb = null;
-    // Start is called before the first frame update
+    //variables
+    private float time_to_live = 3f;
     void Start()
     {
+        //propagation
         bulletrb = GetComponent<Rigidbody2D>();
         bulletrb.velocity = transform.right * bullet_speed;
+        //auto destruction
+        Destroy(gameObject, time_to_live);
     }
 }
