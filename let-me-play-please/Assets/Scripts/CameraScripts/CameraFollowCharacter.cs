e7ed4dc4cf801719c62850f1e87dd908a7eed98d
fhps <higino.felipe@hotmail.com>
2020-07-11 22:38:50 -0300

    Starting agro range
    Player in middle and trigger that activates enemy agro

--- let-me-play-please/Assets/Scripts/CameraScripts/CameraFollowCharacter.cs
+++ let-me-play-please/Assets/Scripts/CameraScripts/CameraFollowCharacter.cs
@@ -9,7 +9,7 @@ public class CameraFollowCharacter : MonoBehaviour
 
     void Update()
     {
-        float x_position = characterReference.transform.position.x + 3; //-3 is the default character position
+        float x_position = characterReference.transform.position.x; //-3 is the default character position
         Vector3 new_position = gameObject.transform.position;
         new_position.x = x_position;
         gameObject.transform.position = new_position; 
