c34d29a07169cadbd73489f881d1fde035cd8195
fhps <higino.felipe@hotmail.com>
2020-07-12 00:00:39 -0300

    Enemy walking when agro
    Walking simple translating

--- let-me-play-please/Assets/Scripts/enemy_scripts/EnemyBehaviour.cs
+++ let-me-play-please/Assets/Scripts/enemy_scripts/EnemyBehaviour.cs
@@ -7,13 +7,16 @@ public class EnemyBehaviour : MonoBehaviour
     //references
     [SerializeField] private GameObject bulletPrefab = null;
     [SerializeField] private Transform spawnBulletPoint = null;
+    private Rigidbody2D controller;
 
     //properties
-    private float shoot_bpm = 200;
+    private float shoot_bpm = 100;
     private bool is_shooting = false;
+    private bool is_walking = false;
 
     private void Start()
     {
+        controller = GetComponent<Rigidbody2D>();
         InvokeRepeating("SpawnBulletRate", 0f, 60 / shoot_bpm);
     }
 
@@ -29,6 +32,20 @@ public class EnemyBehaviour : MonoBehaviour
     {
         transform.Rotate(0f, 180f, 0f);//look to player
         is_shooting = true;
+        is_walking = true;
+    }
+
+    private void FixedUpdate()
+    {
+        //platformer movement made with Move() method at controller
+        if (is_walking)
+        {
+            float horizontal_movement = -1 * Time.fixedDeltaTime;
+            var new_position = transform.position;
+            new_position.x = new_position.x + horizontal_movement;
+            transform.position = new_position;
+        }
+            
     }
 
 }
