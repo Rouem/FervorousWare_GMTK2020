9dc6d2f258769536cb0f41bbb24e63a16b2ba6e6
fhps <higino.felipe@hotmail.com>
2020-07-12 12:21:41 -0300

    Try again working

--- let-me-play-please/Assets/Scripts/GameMannagement/GameLoop.cs
+++ let-me-play-please/Assets/Scripts/GameMannagement/GameLoop.cs
@@ -8,10 +8,12 @@ public class GameLoop : MonoBehaviour
     //references
     [SerializeField] ScreenMannager screens = null;
     [SerializeField] CharacterLife character = null;
+    [SerializeField] PressEnterToTryAgain try_again = null;
 
     private void Start()
     {
         character.die_event += LooseGame;
+        try_again.try_again_event += RestartEverything;
     }
     //Win condition (reaches the end)
     //--ty for playing
---
+++ let-me-play-please/Assets/Scripts/ShowScreen/ScreensBehaviours/PressEnterToTryAgain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressEnterToTryAgain : MonoBehaviour
+{
+    public Action try_again_event; 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            try_again_event();
+        }
+    }
+}
