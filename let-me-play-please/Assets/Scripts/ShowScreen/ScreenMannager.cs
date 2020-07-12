6d134fadde5357f2de17d4e2b872074197bfd640
fhps <higino.felipe@hotmail.com>
2020-07-12 11:39:57 -0300

    Screen mannager
    Screen mannager shows and hides screens

---
+++ let-me-play-please/Assets/Scripts/ShowScreen/ScreenMannager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenMannager : MonoBehaviour
+{
+    //references
+    [SerializeField] ShowScreen start_screen = null;
+    [SerializeField] ShowScreen loose_screen = null;
+    [SerializeField] ShowScreen win_screen = null;
+
+    //variables
+    private ShowScreen active_screen = null;
+
+    private void Start()
+    {
+        ShowStartScreen();
+    }
+
+    public void ShowStartScreen()
+    {
+        active_screen?.HideTheScreen();
+        start_screen?.ShowTheScreen();
+        active_screen = start_screen;
+    }
+
+    public void ShowLooseScreen()
+    {
+        active_screen?.HideTheScreen();
+        loose_screen?.ShowTheScreen();
+        active_screen = loose_screen;
+    }
+
+    public void ShowWinScreen()
+    {
+        active_screen?.HideTheScreen();
+        win_screen?.ShowTheScreen();
+        active_screen = win_screen;
+    }
+}
+
---
+++ let-me-play-please/Assets/Scripts/ShowScreen/ShowScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowScreen : MonoBehaviour
+{
+    public void HideTheScreen()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ShowTheScreen()
+    {
+        gameObject.SetActive(true);
+    }
+}
